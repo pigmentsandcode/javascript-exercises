994812d1c0ccf85719308a79ac98cf4bcccb2eda
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-02-28 17:45:38 -0600

    Add logic to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (strToReverse) {
+  if (strToReverse === "") {
+    return strToReverse;
+  }
+  let strReverseArr = strToReverse.split("");
+  strReverseArr.reverse();
+  return strReverseArr.join("");
 };
 
 // Do not edit below this line
