475dc5e279c4c14edb28e754059051db0755d6e3
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-02 14:34:02 -0600

    Add logic to remove values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (originalArr, ...valuesToRemove) {
+  const newArray = originalArr.filter((val) => {
+    return !valuesToRemove.includes(val);
+  });
+
+  return newArray;
+
+  //for each value in originalArr
+  //check if exist in valuesToRemove
+  //if no, add to newArray
 };
 
 // Do not edit below this line
