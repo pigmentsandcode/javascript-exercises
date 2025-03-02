d970ca436dbcd5af8ecacbfbb48c13c904f7ac90
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-02 14:49:45 -0600

    Add logic to covert between Fahrenheit and Celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  const cTemp = (fTemp - 32) / 1.8;
+  if (!Number.isInteger(cTemp)) {
+    return Number(cTemp.toFixed(1));
+  }
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  const fTemp = cTemp * 1.8 + 32;
+  if (!Number.isInteger(fTemp)) {
+    return Number(fTemp.toFixed(1));
+  }
+  return fTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
