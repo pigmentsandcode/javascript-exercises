14ff8d3afb33b979bca2541d7eedf1aa5750e202
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-05 15:18:20 -0600

    Add logic to calc fibonacci numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  if (num == 0) {
+    return 0;
+  }
 
+  let first = 0;
+  let second = 1;
+
+  for (let nth = 2; nth <= num; nth++) {
+    let sum = first + second;
+    first = second;
+    second = sum;
+  }
+  return second;
 };
 
 // Do not edit below this line
