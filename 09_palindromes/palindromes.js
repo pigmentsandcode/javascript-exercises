25835bdbd068c8470acd3b808e2da76fce1a7d59
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-05 15:10:35 -0600

    Add logic to determine if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+function isCharLetter(char) {
+  return /[a-zA-Z0-9]/.test(char);
+}
 
+const palindromes = function (str) {
+  const charArr = str.split("");
+  const filteredArr = charArr.filter((char) => {
+    return isCharLetter(char);
+  });
+  const reversed = filteredArr.toReversed();
+
+  let isPalindrome = true;
+
+  for (let i = 0; i < filteredArr.length; i++) {
+    if (filteredArr[i].toLowerCase() !== reversed[i].toLowerCase()) {
+      isPalindrome = false;
+      break;
+    }
+  }
+  return isPalindrome;
 };
 
 // Do not edit below this line
