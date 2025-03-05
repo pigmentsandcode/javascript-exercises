f03054e2233a5a50997d5444cb227c13bc91c306
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-03-05 15:47:58 -0600

    Add logic to determine oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+function getAge(person) {
+  let currAge;
+  if (Object.hasOwn(person, "yearOfDeath")) {
+    currAge = person.yearOfDeath - person.yearOfBirth;
+  } else {
+    //is still alive
+    const currentYear = new Date().getFullYear();
+    currAge = currentYear - person.yearOfBirth;
+  }
+  return currAge;
+}
 
+const findTheOldest = function (peopleArr) {
+  const oldestObj = peopleArr.reduce((currOldest, currPerson) => {
+    let currAge = getAge(currPerson);
+    let oldestAge = getAge(currOldest);
+
+    if (currAge > oldestAge) {
+      return currPerson;
+    }
+    return currOldest;
+  });
+  return oldestObj;
 };
 
 // Do not edit below this line
